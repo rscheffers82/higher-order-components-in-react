bde60b7d9177cac45888f832ac18f8f2c3c60302
Roy Scheffers <r.scheffers@gmail.com>
2016-12-31 16:22:20 -0600

    Add re-routes to the app to prevent certain paths to be viewed when not logged in

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -3,6 +3,23 @@ import { connect } from 'react-redux';
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
+    // make the router object available on the Authentication class, not just an instance of it.
+    static contextTypes = {
+      router: React.PropTypes.object
+    }
+
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
     render () {
       console.log('Authenticated: ', this.props.authenticated);
       return <ComposedComponent {...this.props} />
